4c52fa0984390970c81bc98e70171156e30053b5
王　裕恒 <01014592@CA1278.local>
2016-06-28 18:59:38 +0900

    Add fake data and render function

---
+++ src/data/fullrate.js
@@ -0,0 +1,29 @@
+var dummy_data = [
+    {
+        "code": 331,
+        "sections" :[
+            {"code":3310100,"color":"00afef","station1_code":22849,"station2_code":22583,"fullrate":10},
+            {"code":3310110,"color":"00afef","station1_code":22583,"station2_code":22790,"fullrate":30},
+            {"code":3310120,"color":"00afef","station1_code":22790,"station2_code":23053,"fullrate":50},
+            {"code":3310130,"color":"00afef","station1_code":23053,"station2_code":22591,"fullrate":80},
+            {"code":3310140,"color":"00afef","station1_code":22591,"station2_code":22507,"fullrate":90},
+            {"code":3310150,"color":"00afef","station1_code":22507,"station2_code":22645,"fullrate":100},
+            {"code":3310160,"color":"00afef","station1_code":22645,"station2_code":22797,"fullrate":110},
+            {"code":3310170,"color":"00afef","station1_code":22797,"station2_code":22564,"fullrate":120},
+            {"code":3310180,"color":"00afef","station1_code":22564,"station2_code":22884,"fullrate":140},
+            {"code":3310190,"color":"00afef","station1_code":22884,"station2_code":22614,"fullrate":160},
+            {"code":3310200,"color":"00afef","station1_code":22614,"station2_code":23024,"fullrate":180},
+            {"code":3310210,"color":"00afef","station1_code":23024,"station2_code":22638,"fullrate":200},
+            {"code":3310220,"color":"00afef","station1_code":22638,"station2_code":22830,"fullrate":180},
+            {"code":3310230,"color":"00afef","station1_code":22830,"station2_code":22994,"fullrate":140},
+            {"code":3310240,"color":"00afef","station1_code":22994,"station2_code":22868,"fullrate":130},
+            {"code":3310250,"color":"00afef","station1_code":22868,"station2_code":22592,"fullrate":120},
+            {"code":3310260,"color":"00afef","station1_code":22592,"station2_code":22206,"fullrate":110},
+            {"code":3310270,"color":"00afef","station1_code":22206,"station2_code":22453,"fullrate":100},
+            {"code":3310280,"color":"00afef","station1_code":22453,"station2_code":22265,"fullrate":90},
+            {"code":3310290,"color":"00afef","station1_code":22265,"station2_code":29305,"fullrate":80},
+            {"code":3310300,"color":"00afef","station1_code":29305,"station2_code":22407,"fullrate":70},
+            {"code":3310310,"color":"00afef","station1_code":22407,"station2_code":22401,"fullrate":60}],
+
+    },
+]
--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,93 @@ function init() {
 		zoom: 16,   // ズームレベル
 		sideMenuControl: true
     });
-	rosenEventSetter();
+    setSearchBox();
+    //rosen.getSectionsByLineCode(331).then(function(sections){
+    //    console.log(JSON.stringify(sections));
+    //});
+    renderFullrate();
+    //rosen.highlightSections([3310210]);
+	//rosenEventSetter();
+    setLineSelector();
 }
+function renderFullrate() {
+    dummy_data[0].sections.forEach(function (section) {
+        var fcolor = rate2color(section.fullrate);
+        rosen.highlightSections([section.code], {color: fcolor});
+    });
+}
+function readFullrate() {}
+
+function rate2color(val) {
+    var rateMax = 200;
+    var rateMin = 0;
+    if (val > rateMax) {
+        val = rateMax;
+    }
+    else if (val < rateMin) {
+        val = rateMin;
+    }
+    var h = Math.floor((rateMax - val) * 120 / rateMax); //Hue(h) will change from red to green corresponding to rateMax to 0.
+    var s = 1;
+    var v = 1;
+    //hsv to rgb , refer to http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
+    var rgb, i, data = [];
+    h = h / 60;
+    i = Math.floor(h);
+    data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
+    switch(i) {
+        case 0:
+            rgb = [v, data[2], data[0]];
+            break;
+        case 1:
+            rgb = [data[1], v, data[0]];
+            break;
+        case 2:
+            rgb = [data[0], v, data[2]];
+            break;
+        case 3:
+            rgb = [data[0], data[1], v];
+            break;
+        case 4:
+            rgb = [data[2], data[0], v];
+            break;
+        default:
+            rgb = [v, data[0], data[1]];
+            break;
+    }
+    return '#' + rgb.map(function(x){
+        return ("0" + Math.round(x*255).toString(16)).slice(-2);
+    }).join('');
+}
+
+function setSearchBox() {
+    $('#search_input').autocomplete({
+        autoFocus: true,
+        delay: 200,
+        minLength: 1,
+        source: function(req, res) {
+            var search_params = {
+                stationName: req.term,
+                stationNameYomi: true,
+                limit: 10
+            };
+            rosen.searchStations(search_params).then(function(stations) {
+                var data = stations.map(function(s) { return {label: s.name, value: s.code}; });
+                res(data);
+            }).catch(function(err) {
+                res("エラーが発生しました");
+                console.log("err", err);
+            });
+        },
+        select: function( event, ui ) {
+            var station_code = ui.item.value;
+            rosen.setCenterByStationCode(station_code);
+            ui.item.value = ui.item.label;
+        }
+    });
+    console.log($('#search_input').length);
+}
+
 function rosenEventSetter() {
 	//setLineSelector();
 	setSectionSelector();
@@ -21,11 +106,33 @@ function setLineSelector() {
 			if (selectedSections)
 			selectedSections.push(line);
 			msg += line.name + "\n";
-			console.log(line);
+			//console.log(line);
 			rosen.highlightLine(line.code);
+            getFullrateByLineCode(line.code);
 		});
 		$('#map_message').text(msg);
 	});
+}
+
+function getFullrateByLineCode(c) {
+    var line_data = {};
+    rosen.getLineByCode(c).then(function(line){
+        console.log(line);
+        line_data.name = line.name;
+        line_data.code = line.code;
+    });
+    rosen.getSectionsByLineCode(c).then(function(sections){
+        //console.log(JSON.stringify(sections));
+        line_data.sections = sections;
+        //console.log(sections);
+    });
+    line_data.sections.forEach(function (section) {
+        
+    });
+    console.log(line_data);
+}
+function genFakeRate() {
+    return Math.floor(Math.random() * 200);
 }
 
 function setSectionSelector() {
@@ -34,21 +141,25 @@ function setSectionSelector() {
         console.log(data);
 		data.sections.forEach(function (section) {
 			if (selectedSections.includes(section)) {
-                rosen.unhighlightSections([section.code]);
+                rosen.unsetSectionPopup(section.code);
+                //rosen.unhighlightSections([section.code]);
                 while (selectedSections.indexOf(section) !== -1) {
                     selectedSections.splice(selectedSections.indexOf(section), 1);
                 }
 			}
 			else {
+                var popup = Rosen.textPopup().setComment("Section code: " + section.code + "\n");
                 selectedSections.push(section)
 				msg += "Section code: " + section.code + "\n";
 				console.log(section);
-				rosen.highlightSections([section.code]);
+                rosen.setSectionPopup(section.code, popup);
+				//rosen.highlightSections([section.code]);
 			}
 		});
 		$('#map_message').text(msg);
         console.log(selectedSections);
 	});
 }
+
 
 window.addEventListener('load', init);
